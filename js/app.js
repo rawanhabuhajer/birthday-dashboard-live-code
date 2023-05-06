fc8d1373f0ea796fb18e0e8afc7f557a7c4aa62b
rawan6922&9 <rawanh.abuhajer@gmail.com>
2023-05-06 19:59:05 +0300

    add task

--- js/app.js
+++ js/app.js
@@ -2,47 +2,76 @@ let profiles = [
    {
         username: 'eva',
         gender : 'female',
-        age: 24,
-        birthday: '04 February 1990',
+        birthday: '07 February 1990',
     },
     {
         username: 'Mohammad',
         gender : 'Male',
-        age: 24,
-        birthday: '04 May 1990',
+        birthday: '05 May 1998',
     },
     {
         username: 'Majd',
         gender : 'Female',
-        age: 24,
-        birthday: '04 May 1990',
+        birthday: '07 May 1990',
     },
     {
         username: 'Rawan',
         gender : 'Female',
-        age: 24,
-        birthday: '05 May 1990',
+        birthday: '06 May 1990',
     }
 ]
 
-let profilesContainer = document.getElementById('birthday__profile-container')
+let profilesContainer = document.getElementById('birthday__profile-container');
 let currentDate= new Date();
 let  month = currentDate.toLocaleString('default', { month: 'long' });
 let day = currentDate.getDay();
-console.log(day)
-console.log(month);
-console.log(currentDate);
+
+function calculateAge(birthday) { 
+  const birthDate = new Date(birthday);
+  const currentDate = new Date();
+  
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+ 
+  return age;
+}
+
 for(let profile of profiles){
- let  birthday = profile.birthday.split(' ')
-    if(birthday[1] === month && birthday[0] == day ){
+    let birthday = profile.birthday.split(' ');
+    if (birthday[1] === month && birthday[0] == day) {
+      profile.age = calculateAge(profile.birthday);
     //Way 1 : The easy way
+    let birthdayCount = 0;
+    if(profile.gender === "Male") 
+      imgSrc = "assets/img/male.png";
+      else{
+        imgSrc = "assets/img/female.png";
+      }
+    
+      birthdayCount++;
+
         profilesContainer.innerHTML += ` <div class="birthday__profile">
-        <img src="assets/img/female.png" alt="">
+        <img src="${imgSrc}" alt="">
         <div class="birthday__info">
             <p>${profile.username}</p>
             <p> ${profile.age}</p>
         </div>
     </div>`
+   
+    document.getElementById('birthday__counter').innerHTML = `${birthdayCount} Birthday today`;
+
+    document.getElementById("btn__clear").addEventListener("click", clear);
+    function clear() {
+    document.getElementById("birthday__profile-container").style.display='none';
+}
+
+    
+    }
+
+
+}
+
+
+
     //Way 2 : The hard way
         //  let profileCard =  document.createElement('div')
         // profileCard.className='birthday__profile';
@@ -60,6 +89,3 @@ for(let profile of profiles){
         //age.appendChild(document.createTextNode(profile.age))
         //birthdayInfo.appendChild(age)
         //console.log(profile)
-    }
-}
-   
